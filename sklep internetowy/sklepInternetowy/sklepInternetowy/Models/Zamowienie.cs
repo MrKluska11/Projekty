1c6fb86b0f8e953edabcf699eea7caf93f86e2e1
MrKluska11 <kamil13@poczta.onet.eu>
2017-10-04 14:24:13 +0200

    Dodanie modelu danych

---
+++ sklep internetowy/sklepInternetowy/sklepInternetowy/Models/Zamowienie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sklepInternetowy.Models
+{
+    public class Zamowienie
+    {
+        public int ZamowienieId { get; set; }  //klucz główny
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Ulica { get; set; }
+        public string Miasto { get; set; }
+        public string KodPocztowy { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public string Komentarz { get; set; }
+        public DateTime DataZamowienia { get; set; }
+        public StanZamowienia StanZamowienia { get; set; }
+        public decimal WartoscZamowienia { get; set; }
+
+        List<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+    }
+
+    public enum StanZamowienia
+    {
+        Nowe,
+        Zrealizowane
+    }
+}
